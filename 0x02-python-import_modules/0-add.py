31e90306b0069c7e643f2462e16587ee5f118752
Dagnu1 <rasdagnu@gmail.com>
2022-08-25 10:19:47 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    """Print some of 1 and 2"""
-    from add_0 import add
 
+def principal():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    principal()
